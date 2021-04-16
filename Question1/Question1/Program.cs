618790cf7984d4f1d6dae069c7add07544e1fe7e
Ali-Dovrushov <dovrushov7@gmail.com>
2021-04-16 12:49:12 +0400

    Homework from 1 to 4

---
+++ Question1/Question1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("Question №1");
+
+            bool selection = true;
+            bool selectionForSwitch = false;
+
+            do
+            {
+                Console.Write("Enter array elements: ");
+
+                int arrayElements = int.Parse(Console.ReadLine());
+                int[] myArray = new int[arrayElements];
+
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.Write($"Enter {i} index, array number: ");
+                    myArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                int minElement = myArray[0];
+
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    if (myArray[i] < minElement)
+                    {
+                        minElement = myArray[i];
+                    }
+                }
+
+                Console.WriteLine($"Result min element from array: {minElement}");
+                Console.Write("\nWould you try again ? (Y/y) or (N/n): ");
+                do
+                {
+                    char symbolForSwitch = Convert.ToChar(Console.ReadLine());
+                    switch (symbolForSwitch)
+                    {
+                        case 'Y':
+                            {
+                                selection = true;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case 'y':
+                            {
+                                selection = true;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case 'N':
+                            {
+                                Console.WriteLine("Have a good day :)");
+                                selection = false;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case 'n':
+                            {
+                                Console.WriteLine("Have a good day :)");
+                                selection = false;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Write("You enter incorrect symbol, please enter Y/y or N/n: ");
+                                selectionForSwitch = false;
+                                selection = true;
+                                continue;
+                            }
+                    }
+                }
+                while (selectionForSwitch == false);
+            }
+            while (selection == true);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Question2/Question2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question №2");
+
+            bool selection = true;
+            bool selectionForSwitch = false;
+
+            do
+            {
+                Console.Write("Enter array elements: ");
+
+                int arrayElements = int.Parse(Console.ReadLine());
+                int[] myArray = new int[arrayElements];
+                int maxElement = myArray[0];
+
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.Write($"Enter {i} index, array number: ");
+                    myArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    if (myArray[i] > maxElement)
+                    {
+                        maxElement = myArray[i];
+                    }
+                }
+                Console.WriteLine($"Result max element from array: {maxElement}");
+                Console.Write("\nWould you try again ? (Y/y) or (N/n): ");
+                do
+                {
+                    char symbolForSwitch = Convert.ToChar(Console.ReadLine());
+                    switch (symbolForSwitch)
+                    {
+                        case 'Y':
+                            {
+                                selection = true;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case 'y':
+                            {
+                                selection = true;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case 'N':
+                            {
+                                Console.WriteLine("Have a good day :)");
+                                selection = false;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case 'n':
+                            {
+                                Console.WriteLine("Have a good day :)");
+                                selection = false;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Write("You enter incorrect symbol, please enter Y/y or N/n: ");
+                                selectionForSwitch = false;
+                                selection = true;
+                                continue;
+                            }
+                    }
+                }
+                while (selectionForSwitch == false);
+            }
+            while (selection == true);
+
+            Console.ReadKey();
+        }
+    }
+}
